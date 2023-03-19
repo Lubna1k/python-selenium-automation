7c0475724d5795606b7c63412b4879f7526c76fb
lLubna1k <lubna1khan39@yahoo.com>
2023-03-19 13:35:38 -0400

    intro- Algoritms

--- Algo-hw/Intro-Algoritms-1.py
+++ Algo-hw/Intro-Algoritms-1.py
@@ -2,17 +2,17 @@
 # But for multiples of 3 print "Fizz" instead of the number.
 # and for the multiples of five print "Buzz".
 # For numbers which are multiples fo both three and five print "FizzBuzz"
-def fizzbuzz(n: int):
+def Algorithms(n: int):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            print("Algortms")
         elif i % 3 == 0:
-            print("Fizz")
+            print("Algo")
         elif i % 5 == 0:
-            print("Buzz")
+            print("Rithms")
         else:
             print(i)
 
 
 
-fizzbuzz(100) # can't forget this!!
+Algorithms(100) # can't forget this!!
